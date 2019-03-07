d81a12a61f2ba59fe0bdd9ecbc7176f9f13ecd1b
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2019-03-07 13:01:39 +0800

    实现线索二叉树

--- 线索二叉树.c
+++ 线索二叉树.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+typedef enum
+{	
+	/* Link: 0.指向左右孩子指针 Thread:1 表示指向前驱或后驱的线索 */
+	Link,
+	Thread
+}PointerTag;
+
+/*
+* 线索二叉树实现源码
+*/
+int main(int argc,char *argv[])
+{
+	
+	
+
+	return 0;
+}
